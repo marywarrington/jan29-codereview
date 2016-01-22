a945b0d9b8789f3b14c2adfe524951ccccaa68a7
Mary <marywarrington@gmail.com>
2016-01-22 08:52:57 -0800

    Add passing multipleOfThree spec

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,11 @@ var countUp = function(userInput) {
   answer = answer.slice(0, answer.length-1);
   return answer;
 }
+
+var multipleOfThree = function(userInput) {
+  if (userInput % 3 === 0) {
+    return true
+  } else {
+    return false
+  }
+}
