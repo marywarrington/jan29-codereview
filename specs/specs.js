d44c0463b9fe5fe8f4d2f4fc4a9bbbfedd37fcd9
Mary <marywarrington@gmail.com>
2016-01-22 08:54:14 -0800

    Add passing FALSE multipleOfThree spec

--- specs/specs.js
+++ specs/specs.js
@@ -8,4 +8,7 @@ describe('multipleOfThree', function () {
   it("returns true if a number is a multiple of 3", function() {
     expect(multipleOfThree("6")).to.equal(true);
   });
+  it("returns false if a number is not a multiple of 3", function() {
+    expect(multipleOfThree("7")).to.equal(false);
+  });
 });
