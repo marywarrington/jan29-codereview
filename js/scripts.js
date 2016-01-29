f673a2c577891d2e56a014b23206f6e834bfc2b5
Mary <marywarrington@gmail.com>
2016-01-29 08:15:32 -0800

    create object Pizza with size, quantity, toppings

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function Pizza(quantity, size, toppings) {
+  this.quantity = quantity;
+  this.size = size;
+  this.toppings = toppings;
+}
--- specs/specs.js
+++ specs/specs.js
@@ -1,1 +1,9 @@
 
+describe('Pizza', function () {
+  it("will create an object Pizza with the following properties", function() {
+    var newPizza = new Pizza('Quantity', 'Size', 'Toppings');
+     expect(newPizza.quantity).to.equal('Quantity');
+     expect(newPizza.size).to.equal('Size');
+     expect(newPizza.toppings).to.equal('Toppings');
+  });
+});
