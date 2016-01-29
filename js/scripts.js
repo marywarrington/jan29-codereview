36ead2a38ab62fa2e612e2d659beaf7b58815467
Mary <marywarrington@gmail.com>
2016-01-29 13:59:03 -0800

    add js for multiple pizzas ordered at once

--- js/scripts.js
+++ js/scripts.js
@@ -47,6 +47,65 @@ Pizza.prototype.quantityPrice = function() {
 
 
 $(document).ready(function() {
-
-
-})
+  $("#addPizza").click(function() {
+    $("#originalPizza").append('<div class="newPizza">' +
+                                  '<h3>' +
+                                  'New Pizza' +
+                                  '</h3>' +
+                                  '<div class="form-group">' +
+                                    '<label for="quantity">Quantity</label>' +
+                                    '<select id="quantityInput">' +
+                                      '<option value="1">1</option>' +
+                                      '<option value="2">2</option>' +
+                                      '<option value="3">3</option>' +
+                                      '<option value="4">4</option>' +
+                                      '<option value="5">5</option>' +
+                                      '<option value="6">6</option>' +
+                                      '<option value="7">7</option>' +
+                                      '<option value="8">8</option>' +
+                                      '<option value="9">9</option>' +
+                                      '<option value="10">10</option>' +
+                                    '</select>' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                    '<label for="size">Size</label>' +
+                                    '<select id="sizeInput">' +
+                                      '<option value="small">small</option>' +
+                                      '<option value="medium">medium</option>' +
+                                      '<option value="large">large</option>' +
+                                    '</select>' +
+                                  '</div>' +
+                                  '<div class="form-group" id="toppingsInput">' +
+                                    '<label for="toppings">Toppings</label>' +
+                                    '<div class="checkbox">' +
+                                      '<label>' +
+                                        '<input type="checkbox" id="hazelnuts" value="hazelnuts">Hazelnuts' +
+                                      '</label>' +
+                                    '</div>' +
+                                    '<div class="checkbox">' +
+                                      '<label>' +
+                                        '<input type="checkbox" id="extracheese" value="extracheese">Extra Cheese' +
+                                      '</label>' +
+                                    '</div>' +
+                                    '<div class="checkbox">' +
+                                      '<label>' +
+                                        '<input type="checkbox" id="sausage" value="sausage">Sausage' +
+                                      '</label>' +
+                                    '</div>' +
+                                    '<div class="checkbox">' +
+                                      '<label>' +
+                                        '<input type="checkbox" id="brussels" value="brussels">Brussels Sprouts' +
+                                      '</label>' +
+                                    '</div>' +
+                                    '<div class="checkbox">' +
+                                      '<label>' +
+                                        '<input type="checkbox" id="pineapple" value="pineapple">Pineapple' +
+                                      '</label>' +
+                                    '</div>' +
+                                  '</div>' +
+                                '</div>');
+  });
+});
+//   $('form#order').submit(function(event) {
+//
+// });
